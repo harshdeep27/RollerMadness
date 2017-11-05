b2e3039377fc2741a19f96d0cbae143b48a1f87f
harshdeep27 <harshdeep271106@gmail.com>
2017-11-05 17:14:10 +0530

    Add files via upload

---
+++ Scripts/MyHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyHealth : MonoBehaviour {
+	public bool damageOnTrigger = true;
+	public float heal = 10f;
+	public bool damageOnCollision = false;
+	public GameObject mygo; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per fram
+		void OnCollisionEnter(Collision collision) 						// this is used for things that explode on impact and are NOT triggers
+		{	
+		if (damageOnTrigger||damageOnCollision) {
+				if (this.tag == "Player" && collision.gameObject.tag == "Player")	// if the player got hit with it's own bullets, ignore it
+					return;
+
+				if (collision.gameObject.GetComponent<Health> () != null) {	// if the hit object has the Health script on it, deal damage
+				collision.gameObject.GetComponent<Health> ().ApplyHeal (heal);
+		
+				}
+			}
+		}
+
+
+}
